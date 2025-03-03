33d01495b1d58faa1abd81726b43901ed44db997
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-03-03 20:38:38 +0300

    feat: Added docker and base fastapi app

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM debian:stable-slim
+
+RUN apt update
+RUN apt upgrade -y
+RUN apt install -y curl
+RUN curl -LsSf https://astral.sh/uv/install.sh | sh
+
+ENV UV_COMPILE_BYTECODE=1
+ENV UV_LINK_MODE=copy
+ENV UV_PYTHON_PREFERENCE=only-managed
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app/
+
+COPY ./pyproject.toml ./
+RUN $HOME/.local/bin/uv python install
+RUN $HOME/.local/bin/uv sync --no-dev
+
+COPY ./src/ ./src/
+COPY ./alembic.ini ./
+COPY ./alembic/ ./alembic/
+COPY ./.env ./
+
+CMD ["sh", "-c", "export PATH=$PATH:$HOME/.local/bin/ && uv run --no-dev alembic upgrade head && uv run --no-dev gunicorn src.main:app -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 -w $(nproc)"]
--- src/main.py
+++ src/main.py
@@ -1,1 +1,31 @@
 """Test task for web pages."""
+
+
+from os import environ
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+load_dotenv()
+
+
+POSTGRES_USER = environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = environ["POSTGRES_PASSWORD"]
+POSTGRES_DB = environ["POSTGRES_DB"]
+DB_PORT = environ["DB_PORT"]
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgresql:{DB_PORT}/{POSTGRES_DB}"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+Session = async_sessionmaker(engine)
+
+app = FastAPI()
+
+
+@app.get("/help/")
+async def get_help() -> dict[str, str]:
+    """Show info about service."""
+    return {
+        "status": "ok",
+        "help": "This is a service to manage posts",
+    }
